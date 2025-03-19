b9567a4939079c503a098cd832e95492692bc4c4
LouisB <louis.barboutie@gmail.com>
2025-03-19 10:22:46 +0100

    Created repository + Added gitignore + Added introduction script

---
+++ src/hello.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+samples = 100
+x = np.random.rand(samples)*2 - 1
+y = np.random.rand(samples)*2 - 1
+
+r = np.sqrt(x**2 + y**2)
+pi = 4 *np.sum(r < 1)/samples
+print(f'{samples=} gives {pi=} (error {np.pi - pi})')
