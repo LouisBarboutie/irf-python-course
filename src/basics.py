251e2c3fdf6d2de79e82a12a8c5a3464fefac2f2
LouisB <louis.barboutie@gmail.com>
2025-03-23 18:28:01 +0100

    Move functions to separate package

--- src/basics.py
+++ src/basics.py
@@ -3,9 +3,10 @@ Remember to install the local package with:
     > py -m pip install -e . 
 from the directory where the .toml file is located
 """
-import matplotlib.pyplot as plt
-import numpy as np
-import example_package_louis.stringutils as pkg
+
+import tutorialpackage.examplemodules as pkg
+#from tutorialpackage.examplemodules import string, plot, io
+
 
 """
     Own package utilization
@@ -14,21 +15,21 @@ import example_package_louis.stringutils as pkg
 STR1 = 'VOG!lo olH'
 STR2 = 'el,Wrd ROY' 
 
-res = pkg.stringutils.interleave(STR1, STR2)
+res = pkg.string.interleave(STR1, STR2)
 print(res)
+
+
 
 """
     File I/O
 """
-with open('testfile.bin', 'wb') as file:
-    text = 'This is the first line\nThis is the second line'
-    content = text.encode(encoding='utf-8', errors='strict')
-    file.write(content)
 
-with open('testfile.bin', 'rb') as file:
-    content = file.read()
-    text = content.decode(encoding='utf-8', errors='strict')
-    print(text)
+path = 'testfile.bin'
+text = 'This is the first line\nThis is the second line'
+
+pkg.io.binarywrite(path, text)
+content = pkg.io.binaryread(path)
+print(content)
 
 
 
@@ -36,14 +37,4 @@ with open('testfile.bin', 'rb') as file:
     Plotting
 """
 
-# Generate the data for the plot.
-x = np.linspace(0, 2*np.pi, 100)
-y = np.sin(x)
-
-# Setup and display the plot.
-fig, ax = plt.subplots()
-fig.suptitle('A simple sine wave')
-ax.plot(x, y)
-ax.set_xlabel('Angle [rad]')
-ax.set_ylabel('Amplitude')
-plt.show()
+pkg.plot.quickplot_sine()
